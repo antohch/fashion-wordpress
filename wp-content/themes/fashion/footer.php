6f2eb3a8076c6404c976b5ff08d34f785d5bff27
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-01-07 11:57:40 +0300

    поделил на header and footer

---
+++ wp-content/themes/fashion/footer.php
@@ -0,0 +1,23 @@
+<div class="footer-wrapper">
+	<div class="footer-main">
+		<div class="footer-left">
+			<a href="#"><img src="<?php bloginfo('template_url');?>/images/logo-footer.png" /></a>
+			<p>copyright 2011 <a href="#">www.yoururl.com</a></p>
+		</div>
+		<div class="footer-right">
+			<a href="#"><img src="<?php bloginfo('template_url');?>/images/twitter.png" alt="" /></a>
+			<a href="#"><img src="<?php bloginfo('template_url');?>/images/facebook.png" alt="" /></a>
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/jquerypp.custom.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/jquery.elastislide.js"></script>
+<script type="text/javascript">
+	
+	$( '#carousel' ).elastislide();
+	
+</script>
+<?php wp_footer();?>
+</body>
+</html>  
---
+++ wp-content/themes/fashion/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>" />
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/css/elastislide.css" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/css/custom.css" />
+<script src="<?php bloginfo('template_url');?>/js/modernizr.custom.17475.js"></script>
+
+<title>Fashion photographer</title>
+<?php wp_head();?>
+</head>
+<body>
+<div class="karkas">
+	<div class="header">
+		<a href="/"><img src="<?php bloginfo('template_url');?>/images/logo.png" class="logo" alt="Fashion photographer"/></a>
+		<ul class="menu">
+			<li><a href="#" class="active">HOME</a></li>
+			<li><a href="#">ABOUT ME</a></li>
+			<li><a href="#">PORTFOLIO</a></li>
+			<li><a href="#">EXHIBITIONS</a></li>
+			<li><a href="#">CONTACT ME</a></li>
+		</ul>
+	</div>
