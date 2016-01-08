2fecfa8e861d074e27d32dbe82ffd063b2bf7629
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-01-08 15:23:33 +0300

    single

---
+++ wp-content/themes/fashion/single.php
@@ -0,0 +1,15 @@
+<?php get_header('page'); ?>
+
+    <div class="content-main">
+    
+	<?php if (have_posts()): while(have_posts()): the_post();//проверка на посты; этим циклом выводятся все посты ?>
+		<?php the_content(); ?>
+	<?php endwhile; ?>
+	<?php else: ?>
+	<?php endif; ?>
+       
+        
+    </div>    
+</div>
+
+<?php get_footer('page'); ?>
