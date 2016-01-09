74dfced6749fe465678ff0651171ea805180ccac
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-01-09 16:30:43 +0300

    Контакты

--- wp-content/themes/fashion/contacts.php
+++ wp-content/themes/fashion/contacts.php
@@ -6,9 +6,6 @@ Template Name: Шаблон страницы контакты
 <?php get_header('page'); ?>
     <div class="content-main">
         <div class="content-left">
-			
-			
-                
                 <?php if (have_posts()): while(have_posts()): the_post();//проверка на посты; этим циклом выводятся все посты ?>
                    <h1><?php the_title(); ?></h1>
                     <div class="contact-form">
@@ -19,13 +16,23 @@ Template Name: Шаблон страницы контакты
                 <?php endif; ?>
         </div>
 		<div class="sidebar">
-			<img src="<?php bloginfo('template_url'); ?>/images/cont.jpg" alt="" />
-			<h1>Contact Details</h1>
-			<p class="contact-address">18, Your Street, Yourareaname, Cityname, Countyname, DE1 2WX, United Kingdom.</p>
-			<p class="contact-phone">0800 900 0003</p>
-			<p class="contact-mail">youremail@yoursite.com</p>
-			<p class="contact-liciense">Registered photographer No. 09900-CCC</p>
-			<p>VAT registered business number 455-5559-000</p>
+            <?php if (have_posts()): while(have_posts()): the_post();//проверка на посты; этим циклом выводятся все посты ?>
+                <?php the_post_thumbnail(); ?>
+                <h1>Contact Details</h1>
+                
+                <?php $custom_fields = get_post_custom(get_the_ID());?>
+                <?php foreach($custom_fields as $key => $value){
+                    if(preg_match('#^contact-#', $key)){
+                        echo "<p class='{$key}'>{$value[0]}</p>";
+                    }
+                }
+                if ($custom_fields['business-number'][0]){
+                        echo "<p>{$custom_fields['business-number'][0]}</p>";
+                    }
+                ?>
+            <?php endwhile; ?>
+            <?php else: ?>
+            <?php endif; ?>
 		</div>
     </div>
 </div>
